4a8eba19137706b785abac4d8cce6d450d66bbcd
Leonardo Araujo <leoujooficial@gmail.com>
2023-08-14 22:12:12 -0300

    fix: env handler

--- client/app/api/axios.ts
+++ client/app/api/axios.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const ApiURL = process.env.NEXT_PUBLIC_API_URL;
+const prodApiURL = 'https://vial-calculator-service.onrender.com';
 export const axiosClient = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL,
-	headers: {
-		Accept: "application/json",
-		"Content-Type": "application/json"
-	}
-})
+  baseURL: ApiURL ? ApiURL : prodApiURL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
