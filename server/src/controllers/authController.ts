772873185458874973a98b1418311dee5c1b6c18
Leonardo Araujo <leoujooficial@gmail.com>
2023-08-17 15:41:07 -0300

    separating controller into services

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,32 +1,22 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import User from '../models/User';
-import { generateAuthToken } from '../utils/authUtils';
+import { createUser, findUserByEmail, generateAuthToken, isValidPassword } from './services/authService';
 
 export const signup = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Checking if email is already registered
-    const registeredUser = await User.findOne({ email });
-    if (registeredUser) {
+	 // Checking if user exists
+    const user = await findUserByEmail(email);
+    if (user) {
       return res.status(409).json({ error: 'Email already registered' });
     }
 
-    // Sending token
-    const user = await User.create({
-      email,
-      password: hashedPassword,
-    });
-
-    const token = generateAuthToken(user);
+	 // Creating new user
+    const createdUser = await createUser(email, password);
+    const token = generateAuthToken(createdUser.email);
 
     res.status(201).json({ user, token });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 };
@@ -35,26 +25,21 @@ export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
-
-    // Finding user
+	 // Checking if user exists
+    const user = await findUserByEmail(email);
     if (!user) {
       return res.status(401).json({ error: 'Invalid email or password' });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    // Checking is password matches
-    if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+	 // Checking if user's password is valid
+    const isValidPass = await isValidPassword(password, user.password);
+    if (!isValidPass) {
+      return res.status(401).json({ error: 'Wrong password' });
     }
-
-    // Sending token
-    const token = generateAuthToken(user);
+    const token = generateAuthToken(email);
 
     res.status(200).json({ user, token });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 };
--- server/src/utils/authUtils.ts
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { User } from '../../types/User';
-
-export const generateAuthToken = (user: User) => {
-  return jwt.sign({ email: user.email }, 'my-super-secret-key', {
-    expiresIn: '1h', // Set the token expiration time
-  });
-};
